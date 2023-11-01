8ac7b4365d863397d220119772c87f173baa02ab
rushikesh39 <123180691+rushikesh39@users.noreply.github.com>
2023-11-01 14:47:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,36 @@
-
-const express=require("express")
-const socket=require("socket.io")
+const express = require("express");
+const socket = require("socket.io");
 // const cors=require("cors")
-const app =express();
+const app = express();
 
-const server=app.listen(5000,()=>{
-    console.log("server started on port 5000")
-})
+const server = app.listen(5000, () => {
+  console.log("server started at port 5000");
+});
 
-const io=socket(server,{
-    cors:{
-        origin:"*",
-    }
-})
-io.on("connection",(socketClient)=>{
-    console.log(socketClient.id)
-    socketClient.on("MESSAGE",(clientdata)=>{
-        console.log(clientdata)
-        socketClient.emit("MESSAGE",`message:-${clientdata}`)
-    })
-    socketClient.on('BROADCAST',(clientBroadcast)=>{
-        console.log(clientBroadcast)
-        io.emit("sendBroadcastmsg",`Broadcast message:- ${clientBroadcast}`)
-    })
-    socketClient.on("room",(ClientRoom)=>{
-        console.log(ClientRoom)
-        socketClient.join(ClientRoom)
-        socketClient.emit("JoinSuccess",`member:-${ClientRoom} ${socketClient.id}`)
-        socketClient.on("room",(clientdata)=>{
-            io.to(ClientRoom).emit("room",`group name:- ${clientdata} ${socketClient.id}`)
-        })
-        socketClient.on("groupChat",(data)=>{
-            io.to(ClientRoom).emit("groupChat",`group message:- ${data}`)
-        })
-    })
-})
-app.get("/",(req,res)=>{
-    res.send("Home page")
-})
+const io = socket(server, {
+  cors: {
+    origin: "*",
+  },
+});
+io.on("connection", (socketClient) => {
+  console.log(socketClient.id);
 
+  socketClient.on("room", (ClientRoom) => {
+    console.log("room data", ClientRoom);
+    socketClient.join(ClientRoom);
+    socketClient.emit("JoinSuccess", ClientRoom);
+    socketClient.on("room", (clientdata) => {
+      io.to(ClientRoom).emit(
+        "room",
+        `group name:- ${clientdata} ${socketClient.id}`
+      );
+    });
+    socketClient.on("groupChat", (data) => {
+      console.log(data);
+      io.to(ClientRoom).emit("Chat", data);
+    });
+  });
+});
+app.get("/", (req, res) => {
+  res.send("Home page");
+});
